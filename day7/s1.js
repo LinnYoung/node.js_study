3aceb5fc9c3c1613a1ff1fca98bbb21cc999c768
linn <946692837@qq.com>
2020-06-15 15:29:33 +0800

    fix

---
+++ day7/s1.js
@@ -0,0 +1,13 @@
+// 全局对象
+
+/**
+ * __filename 表示当前正在执行的脚本的文件名。它将输出文件所在位置的绝对路径
+ */
+
+console.log(__filename); ///Users/brilljoy/Space/myGit/node.js_study/day7/s1.js
+
+
+/**
+ * __dirname 表示当前执行脚本所在的目录
+ */
+console.log(__dirname); ///Users/brilljoy/Space/myGit/node.js_study/day7
